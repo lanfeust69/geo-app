56eca0ce274acc00fa6238b240a2e13bdc66c1cb
Jean-Jacques Lafay <jeanjacques.lafay@gmail.com>
2019-02-16 13:07:54 +0100

    Adapt e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -3,7 +3,12 @@ import { browser, by, element } from 'protractor';
 describe('geo-app App', () => {
   it('should display Settings dialog', () => {
     browser.get('/');
-    const settingsButton = element(by.css('div[id=menu-button-div] button'));
+    const menuButton = element(by.css('div[id=menu-button-div] button'));
+    expect(menuButton).toBeTruthy();
+    menuButton.click();
+
+    // fails due to https://github.com/angular/material2/issues/10140
+    const settingsButton = element(by.css('div.mat-menu-content button'));
     expect(settingsButton).toBeTruthy();
     settingsButton.click();
     const settingsOkButton = element(by.css('geo-settings button'));
